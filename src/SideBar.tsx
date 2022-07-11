afee48ecf4ea15dd18bf6cab0175437ff86f8b66
apmfree78 <amit@profitswami.com>
2022-07-10 21:35:38 -0400

    sidebar elements aligned

--- src/SideBar.tsx
+++ src/SideBar.tsx
@@ -1,19 +1,24 @@
 import React from 'react';
+import { BsPlus, BsFillLightningFill, BsGearFill } from 'react-icons/bs';
+import { FaFire, FaPoo } from 'react-icons/fa';
+
+interface Props {
+  icon: JSX.Element;
+}
 
 const SideBar: React.FC = () => {
   return (
     <div className='fixed top-0 left-0 h-screen w-16 flex flex-col bg-gray-900 text-white shadow-lg'>
-      <i>A</i>
-      <i>B</i>
-      <i>C</i>
-      <i>D</i>
-      <i>E</i>
+      <SideBarIcon icon={<FaFire size='28' />} />
+      <SideBarIcon icon={<FaFire size='28' />} />
+      <SideBarIcon icon={<FaFire size='28' />} />
+      <SideBarIcon icon={<FaFire size='28' />} />
     </div>
   );
 };
 
-const SideBarIcon: React.FC = ({ icon }) => {
-  <div className='sidebar-icon'>{icon}</div>;
+const SideBarIcon: React.FC<Props> = ({ icon }) => {
+  return <div className='sidebar-icon'>{icon}</div>;
 };
 
 export default SideBar;
